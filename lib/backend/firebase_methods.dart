84a977273467efa1e81826b65e132596f7b98c2f
naidu199 <141550407+naidu199@users.noreply.github.com>
2024-05-27 23:42:44 +0530

    commit

--- lib/backend/firebase_methods.dart
+++ lib/backend/firebase_methods.dart
@@ -1,16 +1,21 @@
-// import 'package:chat_application/models/message.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:chat_application/models/message.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-// class FireBaseMethods {
-//   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-//   Future<void> addTextMessages(
-//       Message message, String senderId, String receiverId) async {
-//     try {
-//       DocumentReference docref = _firestore
-//           .collection('Users')
-//           .doc(senderId)
-//           .collection('chats')
-//           .doc(receiverId);
-//     } catch (e) {}
-//   }
-// }
+class FireBaseMethods {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  Future<void> createNewChat() async {
+    String chatId = DateTime.now().millisecondsSinceEpoch.toString();
+    Map<String, dynamic> chatData = {"chatId": chatId, "messages": []};
+  }
+
+  Future<void> addTextMessages(
+      Message message, String senderId, String receiverId) async {
+    try {
+      DocumentReference docref = _firestore
+          .collection('Users')
+          .doc(senderId)
+          .collection('chats')
+          .doc(receiverId);
+    } catch (e) {}
+  }
+}
